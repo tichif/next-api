85dac3ab4052d083751e141ce31f767c1aff1238
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-07-26 15:48:00 -0400

    Write server side code to post a feedback

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,32 @@
+import fs from 'fs'
+import path from 'path'
+
 function handler(req, res){
-  res.status(200).json({
-    message: 'this works'
-  })
+  if(req.method === 'POST'){
+    const email = req.body.email
+    const feedback = req.body.feedback
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback
+    }
+
+    const filePath = path.join(process.cwd(),'data','feedback.json')
+    const fileData = fs.readFileSync(filePath)
+    const data = JSON.parse(fileData)
+    data.push(newFeedback)
+    fs.writeFileSync(filePath, JSON.stringify(data))
+
+    return res.status(201).json({
+      message: 'Success',
+      feedback: newFeedback
+    })
+  }else{
+    res.status(200).json({
+      message: 'It works',
+    })
+  }
 }
 
 export default handler
