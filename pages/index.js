cc3242c3c3b075c3ee1491a60c3b1447ca9ec645
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-07-26 15:27:19 -0400

    Prepare the frontend

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import { useRef } from "react"
+
 const HomePage = () => {
-  return <h1>HomePage</h1>
+
+  const emailRef = useRef()
+  const feedbackRef = useRef()
+
+  const submitHandler = e => {
+    e.preventDefault()
+
+    const email = emailRef.current.value;
+    const feedback = feedbackRef.current.value
+  }
+
+  return <div>
+     <h1>HomePage</h1>
+    <form onSubmit={submitHandler}>
+      <div>
+        <label htmlFor="email">Your email address </label> 
+        <input type="email"  id="email" ref={emailRef} />
+      </div>
+      <div>
+        <label htmlFor="feedback">Your feedback </label> 
+        <textarea id="feedback" cols="30" rows="5" ref={feedbackRef}></textarea>
+      </div>
+      <button>Send Feedback</button>
+    </form>
+  </div>
 }
 
 export default HomePage
